e90d5b5ae234a60737e80f66af1fd63968635a37
Benjamin <benjaminlang12@gmail.com>
2016-01-30 00:23:43 -0800

    add ritual

---
+++ GGJGame/Ritual.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "GameObject.h"
+#include "Map.h"
+#include <vector>
+#include <cstdlib>
+#include <math.h>
+class Ritual : public GameObject 
+{
+public:
+	Ritual();
+	~Ritual();
+
+	// Inherited via GameObject
+	void initialize(Map &);
+
+	virtual void release() override;
+
+	virtual void update(float dt, Application *) override;
+
+	virtual void draw(sf::RenderWindow &) override;
+	
+private:
+
+	sf::Texture m_personTexture[5];
+	sf::Sprite m_personSprite;
+	sf::Texture m_fireTexture;
+	sf::Sprite m_fireSprite;
+	int getRandomLocation();
+	const int personNumber = 5;
+};
+
+
+
